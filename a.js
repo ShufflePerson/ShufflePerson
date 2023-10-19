2253a29acc41a498f075e430e8737c50a9798523
DarknessTom <121307743+ShufflePerson@users.noreply.github.com>
2023-10-19 16:48:54 +0300

    Create a.js

---
+++ a.js
@@ -0,0 +1,47 @@
+var elem = document.body;
+var startButton = document.getElementById("startGame");
+
+let lastElem = null;
+
+let leftButtonDown = false;
+let rightButtonDown = false;
+let timeout;
+
+document.addEventListener("mousedown", (event) => {
+  if (event.button === 0) { 
+    leftButtonDown = true;
+  } else if (event.button === 2) {
+    rightButtonDown = true;
+  }
+
+  if (leftButtonDown && rightButtonDown) {
+    timeout = setTimeout(() => {
+			setInterval(function(){
+				if (elem) {
+					elem.style.outline = "none";
+					elem.style.zIndex = "unset";
+				}
+
+				elem = document.querySelectorAll('[data-text="' + document.getElementById("question-box").innerText + '"]')[1];
+
+				elem.style.opacity = 0.5;
+
+				if (elem != lastElem) {
+					if (lastElem != null)
+						lastElem.style.opacity = 1;
+					lastElem = elem;
+				}
+			}, 100);    }, 3000);
+  }
+});
+
+document.addEventListener("mouseup", (event) => {
+  if (event.button === 0) {
+    leftButtonDown = false;
+  } else if (event.button === 2) {
+    rightButtonDown = false;
+  }
+
+  clearTimeout(timeout);
+});
+
